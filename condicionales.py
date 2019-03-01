2ea27b0539af43b3be9492848795bf52977a12cb
jbuenof45401 <jbuenof45401@universidadean.edu.co>
2019-02-28 19:55:31 -0500

    ejercicios clase 28 de febrero 2019: condicionales terminado

--- condicionales.py
+++ condicionales.py
@@ -1,3 +1,5 @@
+import time
+
 def armar_mensajes(numero):
     '''
 
@@ -10,8 +12,8 @@ def armar_mensajes(numero):
     >>> armar_mensajes(-3)
     'El numero es negativo'
 
-    :param numero:
-    :return:
+    :param numero: float el numero flotante a validar
+    :return: str: si es positivo o negativo
     '''
 
     if(numero<0):
@@ -19,5 +21,60 @@ def armar_mensajes(numero):
 
     return 'El numero es positivo'
 
+def quien_es_mayor(edad_uno,edad_dos):
+    '''
+    (num) -> str
 
-# print(armar_mensajes(float(input("numero float"))))
+    indica quien de los dos es mayor
+
+    >>> quien_es_mayor(18,20)
+    'El mayor es el segundo'
+
+    >>> quien_es_mayor(29,19)
+    'El mayor es el primero'
+
+    >>> quien_es_mayor(29,29)
+    'los dos tienen la misma edad'
+
+    :param edad_uno: num: edad primer persona
+    :param edad_dos: num: edad segunda persona
+    :return: str: mensaje si es mayor, menor o tienen la misma edad
+    '''
+
+    mensaje = ''
+
+    if (edad_uno > edad_dos):
+        mensaje = 'El mayor es el primero'
+    elif (edad_uno < edad_dos):
+        mensaje = 'El mayor es el segundo'
+    else:
+        mensaje = 'Ambos tienen la misma edad'
+
+
+    return mensaje
+
+def saludo_segun_hora(hora):
+    '''
+    (num)-> str
+
+     >>> saludo_segun_hora(19)
+     'Buenas noches'
+
+     >>> saludo_segun_hora(1)
+     'Buenos dias'
+
+     >>> saludo_segun_hora(12)
+     'Buenas tardes'
+
+    :param hora: num: la hora actual
+    :return: str: el saludo segun la hora
+    '''
+
+    if(hora<12):
+        return 'Buenos dias'
+    elif(hora>=12 and hora<18):
+        return 'Buenas tardes'
+    else:
+        return 'Buenas noches'
+
+print(saludo_segun_hora(int(time.strftime("%H"))))
--- test_armar_mensajes.py
+++ test_armar_mensajes.py
@@ -5,3 +5,6 @@ class TestArmar_mensajes(TestCase):
     def test_armar_mensajes(self):
         self.assertEqual(f.armar_mensajes(-30),'El numero es negativo')
         self.assertEqual(f.armar_mensajes(30), 'El numero es positivo')
+        self.assertEqual(f.quien_es_mayor(18,20),'El mayor es el segundo')
+        self.assertEqual(f.quien_es_mayor(20, 19), 'El mayor es el primero')
+        self.assertEqual(f.quien_es_mayor(20, 20), 'Ambos tienen la misma edad')
