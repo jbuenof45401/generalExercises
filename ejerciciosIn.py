97d325f2ffb2ab359890f1fb6bc22133d1cb4f33
jbuenof45401 <jbuenof45401@universidadean.edu.co>
2019-03-21 19:45:21 -0500

    comando for y listas

--- ejerciciosIn.py
+++ ejerciciosIn.py
@@ -17,3 +17,23 @@ def siesvocal(vocal):
     '''
 
     return vocal in 'aeiouAEIOU' and len(vocal) == 1
+
+def siesconsonante(consonante):
+    '''
+    str (len(1)) -> bool
+
+    recibe una letra y retorna true si es vocal si no false
+
+    >>> siesconsonante('x')
+    True
+
+    >>> siesconsonante('a')
+    False
+
+
+
+    :param consonante: letra a analizar
+    :return: true si es consonante, false si no
+    '''
+
+    return consonante in 'BCDFGHJKLMNÑPQRSTVWXYZbcdfghjklmnñpqrstvwxyz' and len(consonante) == 1
--- funciones_listas.py
+++ funciones_listas.py
@@ -58,3 +58,72 @@ def doble_queso(pizza):
         print('bored')
 
     return pizza
+
+import ejerciciosIn as vocales
+
+
+def contar_vocales(cadena):
+    '''
+    str-> int: cuenta numero de vocales en una cadena
+
+    >>> contar_vocales('mama')
+    2
+
+    >>> contar_vocales('murcielago')
+    5
+
+    :param cadena:str: palabra con vocales
+    :return: int: numero de vocales en una cadena
+    '''
+    cont = 0
+    for vocal in cadena:
+        if(vocales.siesvocal(vocal)):
+            cont+=1
+    return cont
+
+def contar_consonantes(cadena):
+    '''
+    (list)->int: numero de consonantes
+
+    >>> contar_consonantes('mama')
+    2
+
+    >>> contar_consonantes('perro')
+    3
+
+    :param lista_cadenas: lista de cadenas para contar las consonantes
+    :return: int: numero de consonantes
+    '''
+    cont = 0
+    for consonante in cadena:
+        if vocales.siesconsonante(consonante):
+            cont+=1
+    return cont
+
+
+
+def contartodo(cadenas):
+    '''
+    (list)-> list[int,int]
+
+    cuenta vocales en la primera posicion y cuenta consonantes en la segunda
+
+    >>> contartodo(['mama','perro','sebastian'])
+    [8, 10]
+
+    >>> contartodo(['juan','Sebastian','Bueno','Feria'])
+    [12, 11]
+
+
+    :param cadenas: list: lista de cadenas a analizar
+    :return: list[int,int]: lista de enteros contando vocales y consonantes
+    '''
+    cont=[0,0]
+    for cadena in cadenas:
+        for letra in cadena:
+            if vocales.siesvocal(letra):
+                cont[0]+=1
+            elif vocales.siesconsonante(letra):
+                cont[1]+=1
+
+    return cont
